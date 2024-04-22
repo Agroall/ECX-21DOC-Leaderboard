3caa905b98a82e602db8d73c1a6a93d3202621e8
Agroall <110466247+Agroall@users.noreply.github.com>
2024-04-22 11:49:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ sheetnames = ["backEnd", "dataAnalytics", "dataScience", "dsa", "frontEnd", "pyt
 
 df_list = []
 
+# Fetching data from Sheety API and processing it
 for sheet_name in sheetnames:
   response = requests.get(sheety_url+sheet_name)
   data = response.json()
@@ -34,26 +35,27 @@ for sheet_name in sheetnames:
   df = df.iloc[2:-1, :]
   df_list.append(df)
 
+# Combining the processed data into a single leaderboard
 combined_leaderboard = pd.concat(
     df_list,
     ignore_index=True
 )
-
 combined_leaderboard['Score'] = combined_leaderboard['Score'].astype('int64')
 
+# Handling rows without email by adding their scores to the rows above them
 indices_without_email = combined_leaderboard[combined_leaderboard['Email']==''].index
-
-# For each row without email, add its score to the row above it
 for index in indices_without_email:
     combined_leaderboard.loc[index - 1, 'Score'] += combined_leaderboard.loc[index, 'Score']
 
-# Drop the rows without emails
-combined_leaderboard = combined_leaderboard.drop(indices_without_email).reset_index()
+# Dropping the rows without emails
+combined_leaderboard = combined_leaderboard.drop(indices_without_email).reset_index(drop=True)
 
-# Print the modified dataframe
-combined_leaderboard = combined_leaderboard.sort_values('Score',ascending=False).reset_index()
-combined_leaderboard = combined_leaderboard.drop(columns=['index','level_0'])
-combined_leaderboard
+# Sorting the combined leaderboard by score in descending order
+combined_leaderboard = combined_leaderboard.sort_values('Score', ascending=False).reset_index(drop=True)
+
+# Removing unnecessary index columns and printing the modified dataframe
+combined_leaderboard = combined_leaderboard.drop(columns=['index'])
+print(combined_leaderboard)
 
 # st.sidebar.markdown('''
 # Created By Abatan Ayodeji (Agroall) For Dicey Tech Hackathon.
