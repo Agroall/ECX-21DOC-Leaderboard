628fc60c48c29fe2f56359666974370cfb1b6aba
Agroall <110466247+Agroall@users.noreply.github.com>
2024-04-22 11:18:41 +0100

    Update streamlit_app.py
    Leaderboard basics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Packages
 import streamlit as st
 import pandas as pd
+import requests
 
 
 # Configuration
@@ -14,77 +15,43 @@ st.title("`ECX | 21DOC | Leaderboard`")
 st.markdown(' ')
 st.markdown(' ')
 
+
 # st.sidebar.header("`Dicey Tech Hackathon`")
 
 
-# Platform Metrics Per Year
-st.sidebar.subheader('Yearly Performance Review')
-bar_choice = st.sidebar.selectbox('Select metric', [
-    'Number of Posts',
-    'Impressions',
-    'Engagements'
-])
+sheety_url = "https://api.sheety.co/251448a2cce340bfd433a10be3a376c8/ecx21DocLeaderboard/"
+sheetnames = ["backEnd", "dataAnalytics", "dataScience", "dsa", "frontEnd", "python"]
 
+df = []
 
-st.sidebar.markdown('---')
+for sheet_name in sheetnames:
+  response = requests.get(sheety_url+sheet_name)
+  data = response.json()
+  df = pd.DataFrame.from_dict(data[sheet_name])
+  df['Track'] = sheet_name
+  df.columns = ['Email', 'Name', 'Score', 'Track']
+  df = df[['Name', 'Email', 'Track', 'Score']]
+  df = df.iloc[2:-1, :]
+  df_list.append(df)
 
+combined_leaderboard = pd.concat(
+    df_list,
+    ignore_index=True
+)
 
-# Metrics
-st.markdown('### Performance Summary')
-col1, col2, col3 = st.columns(3)
-col1.metric("Most Used Platform", "Instagram", "27% of posts")
-col2.metric("Most Viewed Platform", "Facebook", "5400 Average Impressions Per Post")
-col3.metric("Most Engaging Platform", "Facebook", "3% Average Engagement Rate Per Post")
+combined_leaderboard['Score'] = combined_leaderboard['Score'].astype('int64')
 
+indices_without_email = combined_leaderboard[combined_leaderboard['Email']==''].index
 
-st.markdown('---')
+# For each row without email, add its score to the row above it
+for index in indices_without_email:
+    combined_leaderboard.loc[index - 1, 'Score'] += combined_leaderboard.loc[index, 'Score']
 
+# Drop the rows without emails
+combined_leaderboard = combined_leaderboard.drop(indices_without_email).reset_index()
 
-# Bar_chart_Section
-st.markdown('### Yearly Performance Review')
-if bar_choice == 'Number of Posts':
-
-    for media in mediums:
-        media['year'] = media.Date.apply(lambda x: x.strftime('%Y'))
-        media.sort_values('Date', inplace=True)
-    
-    post_counts = {}
-    for index, media in enumerate(mediums):
-        post_count_temp = media.value_counts('year').sort_index()
-        post_counts[mediums_list[index]] = post_count_temp
-    
-    media_post_count = pd.DataFrame(post_counts)
-    media_post_count['year'] = media_post_count.index
-    
-    colors = ['rgb(31, 119, 180)', 'rgb(255, 127, 14)', 'rgb(44, 160, 44)',
-              'rgb(214, 39, 40)', 'rgb(148, 103, 189)', 'rgb(140, 86, 75)']
-    
-    data = []
-    for i, medium in enumerate(mediums_list):
-        trace = go.Bar(x=media_post_count['year'], y=media_post_count[medium], name=medium,
-                       marker_color=colors[i % len(colors)])
-        data.append(trace)
-    
-    layout = go.Layout(title='Total Yearly Amount Of Posts For All Media Groups',
-                       xaxis=dict(title='Year'),
-                       yaxis=dict(title='Post Count'),
-                       barmode='group',
-                       legend=dict(
-                            x=0,
-                            y=1.0,
-                            bgcolor='rgba(255, 255, 255, 0)',
-                            bordercolor='rgba(255, 255, 255, 0)'
-                        ),
-                        height=800, # Increase the height of the chart
-                        width=1000 # Increase the width of the chart
-                                      )
-    
-    fig = go.Figure(data=data, layout=layout)
-    fig.show()
-
-
-st.plotly_chart(fig)
-
+# Print the modified dataframe
+print(combined_leaderboard.sort_value('score'))
 
 # st.sidebar.markdown('''
 # Created By Abatan Ayodeji (Agroall) For Dicey Tech Hackathon.
